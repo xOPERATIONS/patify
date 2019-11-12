5a9e542daa8e5870cb244a48ed8a471359eaa241
krisfield <kristopher.s.field@nasa.gov>
2019-11-12 11:37:49 -0600

    cleaup checkStep

--- checklist-procedure.js
+++ checklist-procedure.js
@@ -20,6 +20,9 @@ const tasksDir = path.join(projectDir, 'tasks'); //should be called activityDir 
 const procsDir = path.join(projectDir, 'procedures');
 const imagesDir = path.join(projectDir, 'images');
 
+var indent = 0;
+var outPut = '';
+
 if (!fs.existsSync(tasksDir)) {
 	fs.mkdirSync(tasksDir);
 }
@@ -90,9 +93,64 @@ var task = parseTag("Step");
 fs.writeFileSync(path.join(tasksDir, `${basename}.yml`), tasks);
 
 
+function stepCheck(step){
+    $(step).children().each(function(index, element){
+        var tagType = $(element).prop("tagName");
+        if (tagType == "STEPTITLE") {
+            var text = $(element).find("> Text").text().trim().replace(/\s+/g, ' ');
+            var locationInfo = $(element).find("LocationInfo").text().trim().replace(/\s+/g, ' ');
+            var instruction = $(element).find("Instruction").text().trim().replace(/\s+/g, ' ');
+            //What is navInfo used for?
+            var navInfo = $(element).find("NavInfo").text().trim().replace(/\s+/g, ' ');
+
+            outPut += `${indent}- title: ${text}\n${indent} locationInfo: ${locationInfo}\n${indent} instruction: ${instruction}\n${indent} navInfo: ${navInfo}`;
+        }  else if (tagType == "CLARIFYINGINFO") {
+            var type = $(element).attr("infotype");
+            // Not sure if we need this one.
+            var isNumbered = $(element).attr("isnumbered");
+            var itemid = $(element).attr("itemid");
+            //Might need to pull html for content to get symbols
+            var content = $(element).text().trim().replace(/\s+/g, ' ');
+
+            outPut += `${indent}- ${type}: ${content}\n${indent} isNumbered: ${isNumbered}\n${indent} itemid: ${itemid}`;
+        } else if (tagType == "STEPCONTENT") {
+            var instruction = $(element).find("Instruction").text().trim().replace(/\s+/g, ' ');
+            if (instruction != '') {
+                outPut += `${indent}- step: ${instruction}`;
+            }
+
+            //CHECK FOR IMAGES
+            $(element).children().each( function(index, element){
+                var tagType = $(element).prop("tagName");
+
+                if(tagType == "IMG") {
+                   var alt =  $(element).find("ImageReference").attr("alt");
+                   var height =  $(element).find("ImageReference").attr("height");
+                   var width =  $(element).find("ImageReference").attr("width");
+                   var source = $(element).find("ImageReference").attr('src');
+                   var text = $(element).find("ImageReference > ImageTitle > Text").text().trim().replace(/\s+/g, ' ');
+
+                   outPut += `${indent}- image: ${source}\n${indent} text: ${text}\n${indent} width: ${width}\n${indent} height: ${height}\n${indent} alt: ${alt}`;
+                }
+            });
+
+
+        } else if (tagType == "STEP") {
+            indent += 1;
+            return stepCheck(element);
+
+        }
+
+    });
+
+    return outPut;
+}
+
 function parseTag(tagQuery) {
-    var outPut = '';
+    outPut = ''
+
     $(`ChecklistProcedure > ${tagQuery}`).each(function(index, element){
+
 
         var tagType = $(element).prop("tagName");
 
@@ -100,11 +158,11 @@ function parseTag(tagQuery) {
             var content = $(element).text().trim().replace(/\s+/g, ' ');
             outPut = `schemaVersion: ${content}\n`
         }
-        if (tagType == "AUTHORINGTOOL") {
+        else if (tagType == "AUTHORINGTOOL") {
             var content = $(element).text().trim().replace(/\s+/g, ' ');
             outPut = `authoringTool: ${content}\n`;
         }
-        if (tagType == "METADATA") {
+        else if (tagType == "METADATA") {
             var procType = $(element).attr("proctype");
             var status = $(element).attr("status");
             var date = $(element).find("Date").text().trim().replace(/\s+/g, ' ');
@@ -123,23 +181,22 @@ function parseTag(tagQuery) {
     version: ${version}
     proceCode: ${procCode}\n`;
         }
-        if (tagType == "PROCTITLE") {
+        else if (tagType == "PROCTITLE") {
             var title = $(element).find("text").first().text().trim().replace(/\s+/g, ' ');
             var procNumber = $(element).find("ProcNumber").text().trim().replace(/\s+/g, ' ');
-            outPut = `procedure_name: ${title}
-procedure_number: ${procNumber}\n`;
+            outPut = `procedure_name: ${title}\nprocedure_number: ${procNumber}\n`;
         }
-        if (tagType == "TIMEREQUIREMENT") {
+        else if (tagType == "TIMEREQUIREMENT") {
             var content = $(element).html().trim().replace(/\s+/g, ' ');
             outPut = `timeRequirement: ${content}\n`;
-            //NEED TO FIND OUT IF THIS IS USED AND WHAT THE 
+            //NEED TO FIND OUT IF THIS IS USED AND WHAT THE
         }
-        if (tagType == "PROCEDUREOBJECTIVE") {
+        else if (tagType == "PROCEDUREOBJECTIVE") {
             var content = $(element).text().trim().replace(/\s+/g, ' ');
             outPut =`procedure_objective: ${content}\n`;
         }
 
-        if (tagType == "ITEMIZEDLIST") {
+        else if (tagType == "ITEMIZEDLIST") {
             var label = $(element).find("ListTitle").text().trim().replace(/\s+/g, ' ');
             label = label.toLowerCase().replace(':','').replace(' ','_');
             outPut += (`- ${label}:\n`)
@@ -151,68 +208,14 @@ procedure_number: ${procNumber}\n`;
 
         //Add Tools/Parts Materials
 
-        if (tagType == "STEP") {
-            //FIXME indent var not passing into stepCheck function, so var remains
-            var indent = 0;
-            $(element).each(function stepCheck(step, indent){
-                $(step).children().each(function(index, element, indent){
-                    var tagType = $(element).prop("tagName");
-                    if (tagType == "STEPTITLE") {
-                        var text = $(element).find("> Text").text().trim().replace(/\s+/g, ' ');
-                        var locationInfo = $(element).find("LocationInfo").text().trim().replace(/\s+/g, ' ');
-                        var instruction = $(element).find("Instruction").text().trim().replace(/\s+/g, ' ');
-                        //What is navInfo used for?
-                        var navInfo = $(element).find("NavInfo").text().trim().replace(/\s+/g, ' ');
-
-                        outPut = `${indent}- title: ${text}\n${indent} locationInfo: ${locationInfo}\n${indent} instruction: ${instruction}\n${indent} navInfo: ${navInfo}`;
-                    }  else if (tagType == "CLARIFYINGINFO") {
-                        var type = $(element).attr("infotype");
-                        // Not sure if we need this one.
-                        var isNumbered = $(element).attr("isnumbered");
-                        var itemid = $(element).attr("itemid");
-                        //Might need to pull html for content to get symbols
-                        var content = $(element).text().trim().replace(/\s+/g, ' ');
-
-                        outPut += `${indent}- ${type}: ${content}\n${indent} isNumbered: ${isNumbered}\n${indent} itemid: ${itemid}
-                        ` 
-                    } else if (tagType == "STEPCONTENT") {
-                        var instruction = $(element).find("Instruction").text().trim().replace(/\s+/g, ' ');
-                        if (instruction != '') {
-                            outPut += `${indent}- step: ${instruction}`;
-                        }
-                        
-                        //CHECK FOR IMAGES
-                        $(element).children().each( function(index, element){
-                            var tagType = $(element).prop("tagName");
-                            
-                            if(tagType == "IMG") {
-                               var alt =  $(element).find("ImageReference").attr("alt");
-                               var height =  $(element).find("ImageReference").attr("height");
-                               var width =  $(element).find("ImageReference").attr("width");
-                               var source = $(element).find("ImageReference").attr('src');
-                               var text = $(element).find("ImageReference > ImageTitle > Text").text().trim().replace(/\s+/g, ' ');
-
-                               outPut += `${indent}- image: ${source}\n${indent} text: ${text}\n${indent} width: ${width}\n${indent} height: ${height}\n${indent} alt: ${alt}
-                               `
-                            }
-                        });
-                        
-                    } else if (tagType == "STEP") {
-                        indent += 1;
-                        return stepCheck(element, indent);
-                        
-                    }
-
-                    //FIXMER - not returning step output
-                    return outPut;
-
-                });
-                
-            });
-            
+        else if (tagType == "STEP") {
+            indent += 1;
+            $(element).each(stepCheck);
         }
-        
+
+
     });
+    console.log(outPut);
     return outPut;
-    
+
 }
